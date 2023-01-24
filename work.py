a12d5f0e66ce877bf895438e5e08df0a7b5d06a2
BishalDe <itsbishalde@gmail.com>
2023-01-24 17:13:36 +0530

    added files

---
+++ work.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+import nltk
+nltk.download('stopwords')
+from Questgen import main
+from requests_html import HTMLSession
+session = HTMLSession()
+url = "https://www.bajajfinservhealth.in/articles/sugarcane-juice-benefits" #@param {type:"string"}
+selector = "p" #@param {type:"string"}
+with session.get(url) as r:
+
+    paragraph = r.html.find(selector, first=False)
+
+    text = " ".join([ p.text for p in paragraph])
+print(text)
+
+qe= main.BoolQGen()
+payload = {
+            "input_text":text
+        }
+output = qe.predict_boolq(payload)
+pprint (output)
+
